16ee9fcb5b6053df4860e89a10d055ca02681ef7
Damian <dschlimovich@gmail.com>
2018-07-26 20:13:52 -0300

    BugFix segunda funcion

--- Funciones para medicion - Parte hardware/segunda_funcion.py
+++ Funciones para medicion - Parte hardware/segunda_funcion.py
@@ -1,7 +1,7 @@
 import pymysql.cursors
 
 def escribe(num_de_exp, datos):
-	"""" Recibe como parametros: num_de_exp, [temp1, temp2, temp3, temp4, temp5, tempPh, humity, pH] """
+	"""" Recibe como parametros: num_de_exp, [temp1, temp2, temp3, temp4, temp5, tempPh, tempAmb, humity, pH] """
 		# Connect to the database
 	connection = pymysql.connect(host='localhost',
 	                             user='pi',
@@ -13,7 +13,7 @@ def escribe(num_de_exp, datos):
 	try:
 	    with connection.cursor() as cursor:
 	        # Create a new record
-	        sql = "INSERT INTO `sensedValues`(exp, temp1, temp2, temp3, temp4, temp5, tempPh, humity, pH)  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+	        sql = "INSERT INTO `sensedValues`(exp, temp1, temp2, temp3, temp4, tempPh, tempAmb, humity, pH)  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
 	        cursor.execute(sql, ('num_de_exp, datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6], datos[7]'))
 
 	    # connection is not autocommit by default. So you must commit to save
