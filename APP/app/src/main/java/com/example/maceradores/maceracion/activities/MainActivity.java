57ca6239cd06ec6a55753f4dddfbba2a86e30037
BianchiGiuliano <biangiuliano@gmail.com>
2018-10-31 11:14:21 -0300

    Add card view for the mash list

--- APP/app/src/main/java/com/example/maceradores/maceracion/activities/MainActivity.java
+++ APP/app/src/main/java/com/example/maceradores/maceracion/activities/MainActivity.java
@@ -5,19 +5,37 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.maceradores.maceracion.R;
+import com.example.maceradores.maceracion.adapters.MashListAdapter;
+import com.example.maceradores.maceracion.models.Mash;
+import com.example.maceradores.maceracion.models.MeasureValues;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    //Variables del modelo
+    private List<Mash> mashList;
+
+    // UI elements.
+    //List Mash
     private RecyclerView recyclerView;
+    private RecyclerView.Adapter rvAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    //Current values
     private FloatingActionButton fab;
 
 
@@ -26,14 +44,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //añadir un alertDialog al fab con los valores de las mediciones.
+        //Mostrar todos los tipos de maceraciones que tiene planficado el buen hombre.
+        mashList = hardcodeMashList();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewMash);
+        rvAdapter = new MashListAdapter(mashList, R.layout.item_list_mash, new MashListAdapter.onItemClickListener() {
+            @Override
+            public void onItemClick(Mash mash, int position) {
+                Toast.makeText(MainActivity.this, mash.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setAdapter(rvAdapter);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        //añadir un alertDialog al fab con los valores actuales de los sensores.
         fab = (FloatingActionButton) findViewById(R.id.fabCurrentValues);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAlertCurrentValues("Valores Actuales", null);
+                showAlertCurrentValues();
             }
         });
+    }
+
+    private List<Mash> hardcodeMashList() {
+        final MeasureValues measureAux = new MeasureValues(70, 70, 5.4f, 60, 2,2);
+        final List<MeasureValues> listAux = new ArrayList<MeasureValues>(){{ add(measureAux);}};
+        return new ArrayList<Mash>(){{
+           add(new Mash(0, "Mash 1", listAux));
+            add(new Mash(0, "Mash 2", listAux));
+        }};
     }
 
     @Override
@@ -44,10 +88,9 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    private void showAlertCurrentValues(String title, String message){
+    private void showAlertCurrentValues(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        if(title != null) builder.setTitle(title);
-        if(message != null) builder.setMessage(message);
+        builder.setTitle("Valores Actuales");
 
         View currentValuesView = LayoutInflater.from(this).inflate(R.layout.dialog_current_values, null);
         builder.setView(currentValuesView);
