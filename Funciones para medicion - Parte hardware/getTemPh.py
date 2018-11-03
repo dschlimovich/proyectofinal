8765c71aba876ef3fa22ebfb0b399cfbbbc40277
Damian <dschlimovich@gmail.com>
2018-11-03 12:38:02 -0300

    Working on getTempPh.py 3

--- Funciones para medicion - Parte hardware/getTemPh.py
+++ Funciones para medicion - Parte hardware/getTemPh.py
@@ -9,6 +9,6 @@ datos = primer_funcion.funcion_uno(1)
 
 temp = (datos[0]+datos[1]+datos[2]+datos[3])/4
 ph = datos[6]
-salida = str(temp) + "-" + str(ph)
+salida = str(temp) + "&" + str(ph)
 
 print(salida)
