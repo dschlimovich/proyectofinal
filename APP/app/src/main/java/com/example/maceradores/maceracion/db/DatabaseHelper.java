5c4de9422c4ed3339c5645f6b55dcd769c5fec8e
BianchiGiuliano <biangiuliano@gmail.com>
2018-11-08 10:58:25 -0300

    Add DB Helper

---
+++ APP/app/src/main/java/com/example/maceradores/maceracion/db/DatabaseHelper.java
@@ -0,0 +1,27 @@
+package com.example.maceradores.maceracion.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "maceraciones";
+    private static final int DATABASE_VERSION = 1;
+    
+
+    public DatabaseHelper( Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
