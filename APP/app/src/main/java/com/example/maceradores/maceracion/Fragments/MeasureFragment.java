a0b058d0cf98fd4810d63b9f8d5f65204a89cbf3
BianchiGiuliano <biangiuliano@gmail.com>
2018-12-06 10:34:36 -0300

    load temperature cardview through a function

--- APP/app/src/main/java/com/example/maceradores/maceracion/Fragments/MeasureFragment.java
+++ APP/app/src/main/java/com/example/maceradores/maceracion/Fragments/MeasureFragment.java
@@ -36,36 +36,39 @@ public class MeasureFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        this.tvMeasureTemp = (TextView) getView().findViewById(R.id.textViewMeasureTemp);
+        this.tvMeasureTemp = (TextView) getView().findViewById(R.id.textViewMeasureTemp); //podria estar en el oncreate.
 
-        tvMeasureTemp.append(" Calculado: ");
-        tvMeasureTemp.append("68"); //este debería ser el valor promedio calculado.
-
-        tvMeasureTemp.append(" °C \t\t  Desvío: "); //esto es por una cuestión estetica.
-        tvMeasureTemp.append("2"); //valor desviado respecto a lo planificado.
-
-        tvMeasureTemp.append(" °C \n Planificado: ");
-        tvMeasureTemp.append("68"); //Aca iría el valor planificado.
-
-        tvMeasureTemp.append(" °C \t\t Alerta: ± ");
-        tvMeasureTemp.append("3"); //aca sería valor de desvio
-
-        tvMeasureTemp.append(" °C \n Sensor 1: ");
-        tvMeasureTemp.append("68"); //valor del primer sensor.
-
-        tvMeasureTemp.append(" °C \t\t\t Sensor 3: ");
-        tvMeasureTemp.append("68");
-
-        tvMeasureTemp.append(" °C \n Sensor 2: ");
-        tvMeasureTemp.append("68");
-
-        tvMeasureTemp.append(" °C \t\t\t Sensor 4: ");
-        tvMeasureTemp.append("68");
-        tvMeasureTemp.append(" °C");
-
+        loadTemperatureCardView(68, 0, 68, 3, 68, 68, 68, 68);
 
         chronometer = getView().findViewById(R.id.chronometer);
         chronometer.setBase(SystemClock.elapsedRealtime()); //esto debería ser el tiempo en el que hice la inserción o que tuve la primer medida.
         chronometer.start();
+    }
+
+    private void loadTemperatureCardView(float tPromedio, float desvioObtenido, float tPlanificada, float desvioPlanificado, float t1, float t2, float t3, float t4) {
+        tvMeasureTemp.append(" Calculado: ");
+        tvMeasureTemp.append(String.valueOf(tPromedio)); //este debería ser el valor promedio calculado.
+
+        tvMeasureTemp.append(" °C \t\t  Desvío: "); //esto es por una cuestión estetica.
+        tvMeasureTemp.append(String.valueOf(desvioObtenido)); //valor desviado respecto a lo planificado.
+
+        tvMeasureTemp.append(" °C \n Planificado: ");
+        tvMeasureTemp.append(String.valueOf(tPlanificada)); //Aca iría el valor planificado.
+
+        tvMeasureTemp.append(" °C \t\t Alerta: ± ");
+        tvMeasureTemp.append(String.valueOf(desvioPlanificado)); //aca sería valor de desvio
+
+        tvMeasureTemp.append(" °C \n Sensor 1: ");
+        tvMeasureTemp.append(String.valueOf(t1)); //valor del primer sensor.
+
+        tvMeasureTemp.append(" °C \t\t\t Sensor 3: ");
+        tvMeasureTemp.append(String.valueOf(t3));
+
+        tvMeasureTemp.append(" °C \n Sensor 2: ");
+        tvMeasureTemp.append(String.valueOf(t2));
+
+        tvMeasureTemp.append(" °C \t\t\t Sensor 4: ");
+        tvMeasureTemp.append(String.valueOf(t4));
+        tvMeasureTemp.append(" °C");
     }
 }
