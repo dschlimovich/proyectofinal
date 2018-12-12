c09e95bd1227aa65cbf6dbaa85150d6fe6b677c0
BianchiGiuliano <biangiuliano@gmail.com>
2018-12-12 11:31:20 -0300

    add load ui item stage fragment

--- APP/app/src/main/java/com/example/maceradores/maceracion/adapters/StageFragmentAdapter.java
+++ APP/app/src/main/java/com/example/maceradores/maceracion/adapters/StageFragmentAdapter.java
@@ -1,7 +1,9 @@
 package com.example.maceradores.maceracion.adapters;
 
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,7 @@ import com.example.maceradores.maceracion.models.MeasureInterval;
 import org.w3c.dom.Text;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class StageFragmentAdapter extends RecyclerView.Adapter<StageFragmentAdapter.ViewHolder> {
     private int stageInProgress;
@@ -24,6 +27,7 @@ public class StageFragmentAdapter extends RecyclerView.Adapter<StageFragmentAdap
 
     public StageFragmentAdapter(List<MeasureInterval> intervals, int layout) {
         this.intervals = intervals;
+        Log.d("StageFragmentAdapter", "tamaño de intervalos : " + intervals.size());
         this.layout = layout;
         stageInProgress = 0;
     }
@@ -59,6 +63,7 @@ public class StageFragmentAdapter extends RecyclerView.Adapter<StageFragmentAdap
             this.numberStage = (TextView) itemView.findViewById(R.id.textViewNumberStage);
             this.durationStage = (Chronometer) itemView.findViewById(R.id.chronometerDurationStage);
             this.remainingStage = (Chronometer) itemView.findViewById(R.id.chronometerRemainingStage);
+            this.temperatureStage = (TextView) itemView.findViewById(R.id.textViewTemperatureStage);
             this.phStage = (TextView) itemView.findViewById(R.id.textViewPhStage);
             this.temperatureDecoccionStage = (TextView) itemView.findViewById(R.id.textViewTemperatureDecoccionStage);
 
@@ -68,6 +73,31 @@ public class StageFragmentAdapter extends RecyclerView.Adapter<StageFragmentAdap
             //the interval have an order, no?
             String stage = String.valueOf(interval.getOrder());
             numberStage.append(stage);
+            //la duracion de la etapa esta expresada en minutos.
+            int duration = interval.getDuration();
+            durationStage.setBase( SystemClock.elapsedRealtime() - duration*60000);
+
+            // tiempo restante.
+            int remaining = 0;
+            for( int i = 0; i < interval.getOrder() - 1; i++ ){
+                remaining = remaining + intervals.get(i).getDuration();
+            }
+            remainingStage.setBase( SystemClock.elapsedRealtime() - remaining*60000);
+
+
+            //temperatura + desviacion-
+            StringBuilder builder = new StringBuilder();
+            builder.append(interval.getMainTemperature());
+            builder.append(" ± ");
+            builder.append( interval.getMainTemperatureDeviation() ) ;
+            builder.append( " °C ");
+            temperatureStage.append( builder.toString());
+
+            //ph + desviacion
+            phStage.append( interval.getpH() + " ± " + interval.getPhDeviation());
+
+            //temperatura decoccion
+            temperatureDecoccionStage.append( interval.getSecondTemperature() + " ± " + interval.getSecondTemperatureDeviation() + " °C " );
         }
     }
 }
