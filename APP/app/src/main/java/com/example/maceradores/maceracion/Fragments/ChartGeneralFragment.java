432c7ec6e81e5cbebbf9197ec31cf3faa7e49a30
Damian <dschlimovich@gmail.com>
2018-12-21 19:41:53 -0300

    Trying to fix the IconCharts Size

--- APP/app/src/main/java/com/example/maceradores/maceracion/Fragments/ChartGeneralFragment.java
+++ APP/app/src/main/java/com/example/maceradores/maceracion/Fragments/ChartGeneralFragment.java
@@ -4,28 +4,43 @@ package com.example.maceradores.maceracion.Fragments;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.maceradores.maceracion.R;
 import com.example.maceradores.maceracion.db.DatabaseHelper;
 import com.example.maceradores.maceracion.models.SensedValues;
+import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.CandleData;
+import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.CombinedData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,10 +68,47 @@ public class ChartGeneralFragment extends Fragment {
 
 
         loadCharts(idMash,view);//Carga las Graficas
+        loadBoxPlot(idMash,view,0);//Grafica BoxPlot Temperatura
+        loadBoxPlot(idMash,view,1);//Grafica BoxPlot pH
+        setTypeofChart(1,view);
+
         return view;
     }
 
+    private void setTypeofChart(int chart,View view) {
+        //0 para promedio, 1 para Boxplot
+        TextView tv_lChartTemp = (TextView) view.findViewById(R.id.tv_linechartTemp);
+        LineChart lChartTemp = (LineChart) view.findViewById(R.id.chartTemp);
+        TextView tv_lChartPh = (TextView) view.findViewById(R.id.tv_linechartPh);
+        LineChart lChartPh = (LineChart) view.findViewById(R.id.chartpH);
+        TextView tv_boxplotTemp = (TextView) view.findViewById(R.id.tv_boxplotchartTemp);
+        CombinedChart combinedChartTemp = (CombinedChart) view.findViewById(R.id.candle_stick_chartTemp);
+        TextView tv_boxplotPh = (TextView) view.findViewById(R.id.tv_boxplotchartPh);
+        CombinedChart combinedChartPh = (CombinedChart) view.findViewById(R.id.candle_stick_chartPh);
+//            switch (chart) {
+//                case 0:
+        if(chart ==0) {
+            tv_lChartTemp.setVisibility(View.VISIBLE);
+            lChartTemp.setVisibility(View.VISIBLE);
+            tv_lChartPh.setVisibility(View.VISIBLE);
+            lChartPh.setVisibility(View.VISIBLE);
+            tv_boxplotTemp.setVisibility(View.INVISIBLE);
+            combinedChartTemp.setVisibility(View.INVISIBLE);
+            tv_boxplotPh.setVisibility(View.INVISIBLE);
+            combinedChartPh.setVisibility(View.INVISIBLE);
+        }else if (chart == 1){
+            //              case 1:
+            tv_lChartTemp.setVisibility(View.INVISIBLE);
+            lChartTemp.setVisibility(View.INVISIBLE);
+            tv_lChartPh.setVisibility(View.INVISIBLE);
+            lChartPh.setVisibility(View.INVISIBLE);
+            tv_boxplotTemp.setVisibility(View.VISIBLE);
+            combinedChartTemp.setVisibility(View.VISIBLE);
+            tv_boxplotPh.setVisibility(View.VISIBLE);
+            combinedChartPh.setVisibility(View.VISIBLE);
 
+        }
+    }
 
     private void loadCharts(int idMash,View view){
         tempChart = (LineChart) view.findViewById(R.id.chartTemp);
@@ -108,6 +160,7 @@ public class ChartGeneralFragment extends Fragment {
         tempChart.getDescription().setTypeface(Typeface.DEFAULT_BOLD);
         tempChart.getDescription().setTextSize(12.0f);
 
+
         //tempChart.
         tempChart.invalidate(); //refresh
 
@@ -153,65 +206,182 @@ public class ChartGeneralFragment extends Fragment {
         EnzymesChart.getDescription().setTextSize(12.0f);
 
 
-        //yAxis labels
-        final ArrayList<String> yLabel = new ArrayList<>();
-        yLabel.add("00");
-        yLabel.add("10");
-        yLabel.add("20");
-        yLabel.add("30");
-        yLabel.add("40");
-        yLabel.add("50");
-        yLabel.add("60");
-        yLabel.add("70");
-        yLabel.add("80");
-        yLabel.add("90");
-        yLabel.add("100");
-
 
         YAxis left = EnzymesChart.getAxisLeft();
         left.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
         left.setEnabled(true);
         left.setAxisMinimum(0f);
         left.setDrawGridLines(false);
-        left.setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return yLabel.get((int)value);
-            }
-        });
-
-
 
         EnzymesChart.invalidate(); //refresh
     }
 
-    private List<List<Double>> meanSetsTempPhandEnzymesAct(int idMash) {
-        //Devuelve una Lista de Listas, donde la listas interiores son en orden:
-        //MeanTemp,MeanPh,alfaamilasa,betaamilasa,betaglucanasa y proteasa (Las ultimas 4 son
-        // las activaciones de las enzimas)
-
-        //Primero hacer un select con todos los idExp relacionados a este idMash
-        List<Integer> ListidExp = getAllExperiments(idMash); //Ahora la lista viene validada.
+    private void loadBoxPlot(int idMash,View view,int temp0ph1){
 
 
-        //Armar un Array q tenga tantas filas como idExp.
-        int NumMeasures = getMandatoryNumSensedValues(idMash);
-        float[][] matrizTemp = new float[ListidExp.size()][NumMeasures];//Tantas filas como idExp, y tantas columnas como cant de Mediciones
-        float[][] matrizpH = new float[ListidExp.size()][NumMeasures];//Tantas filas como idExp, y tantas columnas como cant de Mediciones
+        CombinedChart combinedChart = (CombinedChart) view.findViewById(R.id.candle_stick_chartTemp);
 
-
-        //Recorrer los idExp haciendo Select de Sensed Values y ubicarlos en cada Fila de la Matriz.
-        for (int i = 0; i < ListidExp.size(); i++) {
-            List<SensedValues> sensedValues = getSensedValues(ListidExp.get(i));
-            for (int j = 0; j < NumMeasures; j++) {
-                matrizTemp[i][j] = validatedTempMean(sensedValues.get(j).getTemp1(),
-                        sensedValues.get(j).getTemp2(), sensedValues.get(j).getTemp3(), sensedValues.get(j).getTemp4());
-                matrizpH[i][j]=sensedValues.get(j).getpH();
-                Log.d("MatrizTemp celda: "+String.valueOf(i)+","+String.valueOf(j)+"=",String.valueOf(matrizTemp[i][j]));
-                Log.d("MatrizpH celda: "+String.valueOf(i)+","+String.valueOf(j)+"=",String.valueOf(matrizpH[i][j]));
-            }
-            //Validar que la cant de Sensed Values q vienen por IdExp sea == a los q deberia de tener un Exp completo.
+        if(temp0ph1 == 1){//Seteo grafica de pH
+            combinedChart = (CombinedChart) view.findViewById(R.id.candle_stick_chartPh);
         }
+
+        List<List<Float>> medianAndQuartils = getDataforBoxPlot(idMash,temp0ph1);
+        List<CandleEntry> candleEntries = new ArrayList<CandleEntry>();
+        List<Entry> entries =  new ArrayList<>();
+
+        List<Integer> intervalos = intervaloMedicionTempPh(idMash);
+
+        for(int x=0; x<medianAndQuartils.size();x++){
+            candleEntries.add(new CandleEntry(x*(intervalos.get(0)/60),medianAndQuartils.get(x).get(1),
+                    medianAndQuartils.get(x).get(2),
+                    medianAndQuartils.get(x).get(3),
+                    medianAndQuartils.get(x).get(4)));
+
+            entries.add(new Entry(x*(intervalos.get(0)/60),medianAndQuartils.get(x).get(0)));
+        }
+
+        LineDataSet lineDataSet = new LineDataSet(entries,"Median");
+        CandleDataSet candleDataSet = new CandleDataSet(candleEntries,"Box-plot");
+
+        candleDataSet.setColor(Color.rgb(80, 80, 80));
+        candleDataSet.setShadowColor(Color.DKGRAY);
+        candleDataSet.setShadowWidth(0.4f);
+        candleDataSet.setDecreasingColor(Color.RED);
+        candleDataSet.setDecreasingPaintStyle(Paint.Style.FILL);
+        candleDataSet.setIncreasingColor(Color.rgb(122, 242, 84));
+        candleDataSet.setIncreasingPaintStyle(Paint.Style.STROKE);
+        candleDataSet.setNeutralColor(Color.BLUE);
+        candleDataSet.setValueTextColor(Color.RED);
+        lineDataSet.setFillColor(Color.WHITE);
+        lineDataSet.setFillAlpha(0);
+
+        LineData lineData = new LineData(lineDataSet);
+        CandleData candleData = new CandleData(candleDataSet);
+
+
+        CombinedData combinedData = new CombinedData();
+
+        combinedData.setData(lineData);
+        combinedData.setData(candleData);
+
+
+        //-----
+
+        YAxis yAxis = combinedChart.getAxisLeft();
+        YAxis rightAxis = combinedChart.getAxisRight();
+        rightAxis.setEnabled(false);
+        yAxis.setEnabled(true);
+        if(temp0ph1==0) {//Solo toca el eje y si es para grafico de temperatura y no de pH
+            yAxis.setAxisMinimum(20f);
+            yAxis.setAxisMaximum(80f);
+            yAxis.setSpaceTop(50);
+        }
+        else if (temp0ph1 ==1){
+            yAxis.setAxisMinimum(4f);
+            yAxis.setAxisMaximum(6f);
+            yAxis.setSpaceTop(50);
+        }
+
+        XAxis xAxis =  combinedChart.getXAxis();
+        xAxis.setEnabled(true);
+        xAxis.setAxisMinimum(-0.5f);
+
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        combinedChart.setTouchEnabled(true);
+        combinedChart.setHighlightPerDragEnabled(true);
+        combinedChart.setHighlightPerTapEnabled(true);
+
+        combinedChart.setData(combinedData);
+        combinedChart.invalidate();//refresh
+
+    }
+
+    private List<List<Float>> getDataforBoxPlot(int idMash,int temp0ph1){
+        //0=Mediana, 1=max,2=min,3=Q3 y 4=Q1.
+
+        List<Float[][]> matrixTempPh_SV = buildSensedValueMatrix(idMash);
+
+
+        //matrix[0].length gives you the number of columns (assuming all rows have the same length).
+        int cols = matrixTempPh_SV.get(0)[0].length;
+
+        List<List<Float>> columnas = new ArrayList<>();
+
+        List<List<Float>> retorno =  new ArrayList<>();//las filas de la submatriz son la mediana y los cuartiles
+
+        for (int j = 0; j<cols;j++){
+            List<Float> column = getColumn(matrixTempPh_SV.get(temp0ph1),j);//Obtengo la columa del array como una lista
+            Collections.sort(column); //Me ordena de menor a Mayor
+            columnas.add(column); //Columnas para la matrix de Temp por eso get(0)
+
+            //Hasta aca tengo las columnas ordenadas
+
+            float max = Collections.max(column);
+            float min = Collections.min(column);
+            float median=0;
+            float Q1=0;
+            float Q3=0;
+            boolean flagm=true;
+            boolean flagq1=true;
+            boolean flagq3=true;
+            int indexM=0;
+            int indexQ1=0;
+            int indexQ3=0;
+
+            boolean pair = false;
+            if(column.size()%2==0){
+                pair=true;
+            }
+            if(!pair){
+                indexM = (int) Math.ceil((double) column.size() / 2);
+                indexQ1 = (int) Math.ceil((double) column.size() / 4);
+                indexQ3 = (int) Math.ceil((double) column.size()* 3 / 4);
+            }
+            else{
+                indexM = column.size()/2;
+                indexQ1 = column.size()/4;
+                indexQ3 = column.size()*3/4;
+            }
+            median = column.get(indexM);
+            Q1 = column.get(indexQ1-1);//-1 xq trabajo con size y los indices arrancan en cero
+            Q3 = column.get(indexQ3-1);
+
+            Log.d("mediana",String.valueOf(median));
+            Log.d("min",String.valueOf(min));
+            Log.d("Q1",String.valueOf(Q1));
+            Log.d("Q3",String.valueOf(Q3));
+            Log.d("max",String.valueOf(max));
+            List<Float> cuantiles = new ArrayList<>();
+            cuantiles.add(median);
+            cuantiles.add(max);
+            cuantiles.add(min);
+            cuantiles.add(Q3);
+            cuantiles.add(Q1);
+            retorno.add(cuantiles);
+        }
+        return retorno;
+    }
+
+    private List<Float> getColumn(Float[][] matrix, int numCol){
+        List<Float> retorno = new ArrayList<>();
+        for (int i = 0; i<matrix.length;i++) {//Recorre por filas
+            retorno.add(matrix[i][numCol]);
+        }
+        return retorno;
+
+    }
+
+    private List<List<Double>> meanSetsTempPhandEnzymesAct(int idMash) {
+
+        List<Integer> ListidExp = getAllExperiments(idMash); //Ahora la lista viene validada.
+        int NumMeasures = getMandatoryNumSensedValues(idMash);
+
+        List<Float[][]> matrixTempPh = buildSensedValueMatrix(idMash);
+        Float[][] matrizTemp = matrixTempPh.get(0);
+        Float[][] matrizpH = matrixTempPh.get(1);
+
 
         //Calculo Promedio
         List<Double> meanTemp = new ArrayList<>();
@@ -237,12 +407,20 @@ public class ChartGeneralFragment extends Fragment {
         for (int i=0;i<meanTemp.size();i++){
             alphaAmylase.add((double)SensedValues.alphaAmylase( (float)(double) meanTemp.get(i),
                     (float)(double)meanpH.get(i)));
-            betaAmylase.add((double)SensedValues.alphaAmylase( (float)(double) meanTemp.get(i),
+            Log.d("AlfaAmilasa",String.valueOf(SensedValues.alphaAmylase( (float)(double) meanTemp.get(i),//Esto tira ceros
+                    (float)(double)meanpH.get(i))));
+            betaAmylase.add((double)SensedValues.betaAmylase( (float)(double) meanTemp.get(i),
                     (float)(double)meanpH.get(i)));
-            betaGlucanase.add((double)SensedValues.alphaAmylase( (float)(double) meanTemp.get(i),
+            Log.d("betaAmilasa",String.valueOf(SensedValues.alphaAmylase( (float)(double) meanTemp.get(i),//Esto tira ceros
+                    (float)(double)meanpH.get(i))));
+            betaGlucanase.add((double)SensedValues.betaGlucanase( (float)(double) meanTemp.get(i),
                     (float)(double)meanpH.get(i)));
-            protease.add((double)SensedValues.alphaAmylase( (float)(double) meanTemp.get(i),
+            Log.d("BetaGlucanasa",String.valueOf(SensedValues.alphaAmylase( (float)(double) meanTemp.get(i),//Esto tira ceros
+                    (float)(double)meanpH.get(i))));
+            protease.add((double)SensedValues.protease( (float)(double) meanTemp.get(i),
                     (float)(double)meanpH.get(i)));
+            Log.d("Proteasa",String.valueOf(SensedValues.alphaAmylase( (float)(double) meanTemp.get(i),//Esto tira ceros
+                    (float)(double)meanpH.get(i))));
         }
 
 
@@ -255,6 +433,36 @@ public class ChartGeneralFragment extends Fragment {
         retorno.add(protease);
 
         return retorno;
+    }
+
+    private List<Float[][]> buildSensedValueMatrix(int idMash){
+        //Primero hacer un select con todos los idExp relacionados a este idMash
+        List<Integer> ListidExp = getAllExperiments(idMash); //Ahora la lista viene validada.
+
+
+        //Armar un Array q tenga tantas filas como idExp.
+        int NumMeasures = getMandatoryNumSensedValues(idMash);
+        Float[][] matrizTemp = new Float[ListidExp.size()][NumMeasures];//Tantas filas como idExp, y tantas columnas como cant de Mediciones
+        Float[][] matrizpH = new Float[ListidExp.size()][NumMeasures];//Tantas filas como idExp, y tantas columnas como cant de Mediciones
+
+
+        //Recorrer los idExp haciendo Select de Sensed Values y ubicarlos en cada Fila de la Matriz.
+        for (int i = 0; i < ListidExp.size(); i++) {
+            List<SensedValues> sensedValues = getSensedValues(ListidExp.get(i));
+            for (int j = 0; j < NumMeasures; j++) {
+                matrizTemp[i][j] = validatedTempMean(sensedValues.get(j).getTemp1(),
+                        sensedValues.get(j).getTemp2(), sensedValues.get(j).getTemp3(), sensedValues.get(j).getTemp4());
+                matrizpH[i][j]=sensedValues.get(j).getpH();
+                Log.d("MatrizTemp celda: "+String.valueOf(i)+","+String.valueOf(j)+"=",String.valueOf(matrizTemp[i][j]));
+                Log.d("MatrizpH celda: "+String.valueOf(i)+","+String.valueOf(j)+"=",String.valueOf(matrizpH[i][j]));
+            }
+            //Validar que la cant de Sensed Values q vienen por IdExp sea == a los q deberia de tener un Exp completo.
+        }
+
+        List<Float[][]> retorno = new ArrayList<>();
+        retorno.add(matrizTemp);
+        retorno.add(matrizpH);
+        return  retorno;
     }
 
     private List<Integer> getAllExperiments(int idMash) {
